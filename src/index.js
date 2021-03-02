855cc6cf27dcfc32ba90a2b474933bbf642001cf
seathson <ol.sadomov@gmail.com>
2021-03-02 17:14:01 +0300

    optimization

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const store = createStore(rootReducer, compose(
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>,
