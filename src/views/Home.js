84d0a1e39060c84c43f89a12dbf7479408f192b2
seathson <ol.sadomov@gmail.com>
2021-03-02 16:49:35 +0300

    add gh-pages

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,27 +1,15 @@
 import React from "react";
 import Search from "../components/search/Search";
-import BackgroundDay from "../assets/imgs/day.jpg";
-import BackgroundNight from "../assets/imgs/night.png";
 import { useSelector } from "react-redux";
 
 function Home() {
   const mode = useSelector(state => state.theme.light)
   const effect = useSelector(state => state.effect.snow)
 
-  const day = {
-    background: `url(${BackgroundDay})`,
-    transition: "1s",
-  };
-
-  const night = {
-    background: `url(${BackgroundNight})`,
-    transition: "1s",
-  };
-
   return (
-    <div className="container" style={mode ? day : night}>
+    <div className={mode ? 'container' : 'container container_night'}>
       <div className="search">
-        <h1>Search</h1>
+        <h1 className={mode ? 'search__title' : 'search__title search__title_night'}>Search</h1>
         <Search  mode={mode} effect={effect} />
       </div>
     </div>
